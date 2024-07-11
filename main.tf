1fb5e4db4f5e723a9b3fa685dfb7eb25bc40d351
Syed <111910282+cybertron-syed@users.noreply.github.com>
2024-07-11 21:31:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,3 @@
 module "aws-nuke" {
   source                         = "./modules"
-  cloudwatch_schedule_expression = "cron(0 00 ? * FRI *)"
-  older_than                     = "0d"
-  aws_regions                    = ["ap-south-1"]
-  include_resources              = "efs"
-  exclude_resources              = null
-  required_tags                  = "env=develop"
 }
